dfa9669a2d2f49ae54fcee9e3dfb5b0b5e22e6d2
gabrielrez <97976458+gabrielrez@users.noreply.github.com>
2024-03-02 16:32:12 -0300

    refatorado

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const btns = document.querySelectorAll(".game");
-
 const options = [0, 1, 2];
 let selected = 0;
+
 btns[selected].classList.add("active");
+btns.forEach((btn) => {
+  btn.addEventListener("click", handleClick);
+});
+
+function handleClick(event) {
+  event.preventDefault();
+}
 
 window.addEventListener("keydown", changeSelected);
 
@@ -14,7 +21,7 @@ function changeSelected(event) {
     console.log("down");
     selected += 1;
   } else if (event.keyCode == 13) {
-    window.open(btns[selected].url);
+    window.open(btns[selected].href);
   }
   btns.forEach((btn) => {
     btn.classList.remove("active");
