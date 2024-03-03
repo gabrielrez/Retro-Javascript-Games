c08e67ff942acf42488e7ebe41be475d133f3a1e
gabrielrez <97976458+gabrielrez@users.noreply.github.com>
2024-03-02 21:00:32 -0300

    jogo da cobrinha inicio

--- js/snake.js
+++ js/snake.js
@@ -1,5 +1,67 @@
+const snakeColor = "#326234";
 const canvas = document.querySelector(".canvas");
 const ctx = canvas.getContext("2d");
+const size = 25;
+const snake = [
+  { x: 175, y: 175 },
+  { x: 175, y: 200 },
+];
+let direction, loopId;
 
-ctx.fillStyle = "#326234";
-ctx.fillRect(175, 175, 24, 48);
+const drawSnake = () => {
+  ctx.fillStyle = snakeColor;
+  snake.forEach((position, index) => {
+    ctx.fillRect(position.x, position.y, size, size);
+  })
+}
+
+const moveSnake = () => {
+  if (direction) {
+    const head = snake[snake.length - 1];
+
+    if (direction == "right") {
+      snake.push({ x: 0 + head.x + size, y: head.y });
+    }
+
+    if (direction == "left") {
+      snake.push({ x: 0 + head.x - size, y: head.y });
+    }
+
+    if (direction == "up") {
+      snake.push({ x: head.x, y: head.y - size });
+    }
+
+    if (direction == "down") {
+      snake.push({ x: head.x, y: head.y + size });
+    }
+
+    snake.shift();
+  }
+}
+
+const gameLoop = () => {
+  clearInterval(loopId);
+  ctx.clearRect(0, 0, 400, 400);
+  moveSnake();
+  drawSnake();
+  loopId = setTimeout(() => {
+    gameLoop();
+  }, 200);
+};
+
+// gameLoop();
+
+document.addEventListener("keydown", (event) => {
+  if (event.key == "ArrowUp" && direction != "down") {
+    direction = "up";
+  }
+  if (event.key == "ArrowDown" && direction != "up") {
+    direction = "down";
+  }
+  if (event.key == "ArrowLeft" && direction != "right") {
+    direction = "left";
+  }
+  if (event.key == "ArrowRight" && direction != "left") {
+    direction = "right";
+  }
+});
