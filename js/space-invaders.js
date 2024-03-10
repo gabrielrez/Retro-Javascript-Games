828b78476e2531bb485e063886358536c39fed6e
gabrielrez <97976458+gabrielrez@users.noreply.github.com>
2024-03-10 18:58:02 -0300

    shoot space invaders

--- js/space-invaders.js
+++ js/space-invaders.js
@@ -1,39 +1,60 @@
-const spaceShipColor = "#326234";
+const playerColor = "#326234";
+const bulletColor = "#326234";
 const canvas = document.querySelector(".canvas");
 const ctx = canvas.getContext("2d");
 const size = 25;
-var spaceShip = [
+var player = [
   { x: (canvas.width / 2) - size * 2, y: canvas.height - size },
   { x: (canvas.width / 2) - size, y: canvas.height - size },
   { x: canvas.width / 2, y: canvas.height - size },
   { x: (canvas.width / 2) - size, y: canvas.height - size * 2 },
 ]
-let loopId, direction;
+let loopId, direction, canShoot;
+canShoot = true;
+let bullets = [];
 
-function drawSpaceShip() {
-  ctx.fillStyle = spaceShipColor;
-  spaceShip.forEach((position) => {
-    ctx.fillRect(position.x, position.y, size, size);
-  })
-}
-
-function moveSpaceShip() {
-  if (direction == "left" && spaceShip[0].x > 0) {
-    spaceShip.forEach((position) => {
+function movePlayer() {
+  if (direction == "left" && player[0].x > 0) {
+    player.forEach((position) => {
       position.x -= size;
     })
-  } else if (direction == "right" && spaceShip[2].x < canvas.width - size) {
-    spaceShip.forEach((position) => {
+  } else if (direction == "right" && player[2].x < canvas.width - size) {
+    player.forEach((position) => {
       position.x += size;
     })
   }
 }
 
+function moveBullets(){
+  bullets.forEach((bullet, index)=>{
+    bullet.y -= size;
+    if(bullet.y < 0){
+      bullets.splice(index, 1);
+    }
+  })
+}
+
 function drawPlayer() {
-  ctx.fillStyle = spaceShipColor;
-  spaceShip.forEach((position) => {
+  ctx.fillStyle = playerColor;
+  player.forEach((position) => {
     ctx.fillRect(position.x, position.y, size, size);
   })
+
+  ctx.fillStyle = bulletColor;
+  bullets.forEach((bullet)=>{
+    ctx.fillRect(bullet.x, bullet.y, size, size);
+  })
+}
+
+function fireBullet() {
+  if(canShoot){
+    const bullet = { x: player[3].x, y: canvas.height - size - size };
+    bullets.push(bullet);
+    canShoot = false;
+    setTimeout(() => {
+      canShoot = true;
+    }, 300);
+  }
 }
 
 function checkKey(event) {
@@ -41,6 +62,8 @@ function checkKey(event) {
     direction = "left";
   } else if (event.key == "ArrowRight") {
     direction = "right";
+  }else{
+    fireBullet();
   }
 }
 
@@ -48,7 +71,8 @@ function gameLoop() {
   clearInterval(loopId);
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawGrid();
-  moveSpaceShip();
+  movePlayer();
+  moveBullets();
   drawPlayer();
   loopId = setTimeout(() => {
     gameLoop();
